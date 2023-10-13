2bde8d91128c6f458c872faf9e49c14e65f75987
devjiraynor <jiraynor@naver.com>
2023-10-13 10:51:44 +0900

    2023.10.13. 10:50

---
+++ realtime-back/src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  
+  constructor(
+    private readonly appService: AppService
+  ) {}
+
+  @Get()
+  getHello(): string {
+    const reponse: string = this.appService.getHello();
+    return reponse;
+  }
+}
---
+++ realtime-back/src/chat/chat/chat.ts
@@ -0,0 +1,10 @@
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway(4020, { transports: ['websocket', 'polling'], cors: { origin: '*' } })
+export class Chat {
+
+    @WebSocketServer()
+    server: Server;
+
+}
