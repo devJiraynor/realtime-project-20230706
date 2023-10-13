ba6157c2a8847740a9ddbd41b134b6f8f6c27067
devjiraynor <jiraynor@naver.com>
2023-10-13 11:49:03 +0900

    2023.10.13. 11:50

--- realtime-back/src/chat/chat/chat.ts
+++ realtime-back/src/chat/chat/chat.ts
@@ -1,10 +1,36 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Logger } from '@nestjs/common';
+import { SubscribeMessage, WebSocketGateway, WebSocketServer, MessageBody, ConnectedSocket } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+interface MessageDto {
+    room: string;
+    nickname: string;
+    message: string;
+}
 
 @WebSocketGateway(4020, { transports: ['websocket', 'polling'], cors: { origin: '*' } })
 export class Chat {
 
     @WebSocketServer()
     server: Server;
+    logger = new Logger();
+
+    @SubscribeMessage('join')
+    handleJoin(
+        @MessageBody() room: string,
+        @ConnectedSocket() socket: Socket
+    ): void {
+        this.logger.warn(`Join Room : ${room}`);
+        socket.join(room);
+    }
+
+    @SubscribeMessage('send')
+    handlerSend(
+        @MessageBody() messageDto: MessageDto
+    ): void {
+        const { room, message, nickname } = messageDto;
+        this.logger.verbose(message);
+        this.server.to(room).emit('get', { nickname, message });
+    }
 
 }
